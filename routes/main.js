2f9d30eaf428011444c320fc4a19a936cc5bc49f
Junior Silva <jros2.silva@gmail.com>
2019-01-11 00:22:06 -0300

    Rota post criação de gig

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const async = require('async');
+const Gig = require("../models/gig");
+const User = require("../models/user");
 
 router.get('/', (req, res, next) => {
     res.render('main/home');
@@ -11,10 +14,34 @@ router.get('/my-gigs', (req, res, next) => {
 router.route('/add-new-gig')
     .get((req, res, next) => {
         res.render('main/add-new-gig');
+    })
+    .post((req, res, next) => {
+        async.waterfall([
+            function(callback) {
+                var gig = new Gig();
+                gig.owner = req.user._id;
+                gig.title = req.body.gig_title;
+                gig.category = req.body.gig_category;
+                gig.about = req.body.gig_about;
+                gig.price = req.body.gig_price;
+                gig.save(function(err) {
+                    callback(err, gig);
+                });
+            },
+
+            function(gig, callback) {
+                User.update(
+                    {
+                        _id: req.user._id
+                    },{
+                        $push: { gigs: gig._id }
+                    }, function(err, count) {
+                        res.redirect('/my-gigs');
+                    }
+                )
+            }
+        ]);
     });
-
-
-
 
 
 module.exports = router;
