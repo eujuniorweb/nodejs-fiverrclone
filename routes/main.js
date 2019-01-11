b6bd3a9096ee0732b438c50742c34362c3b22587
Junior Silva <jros2.silva@gmail.com>
2019-01-10 23:24:12 -0300

    Formulario de cadastro de gig

--- routes/main.js
+++ routes/main.js
@@ -4,4 +4,17 @@ router.get('/', (req, res, next) => {
     res.render('main/home');
 });
 
+router.get('/my-gigs', (req, res, next) => {
+    res.render('main/my-gigs');
+});
+
+router.route('/add-new-gig')
+    .get((req, res, next) => {
+        res.render('main/add-new-gig');
+    });
+
+
+
+
+
 module.exports = router;
